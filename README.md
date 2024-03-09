11071c8ebb84cac547701f4ff38dd9f5e00af3f4
Kristian Dupont <kristian@kristiandupont.com>
2024-03-09 15:35:35 +0000

    First version

---
+++ README.md
@@ -0,0 +1,41 @@
+# React Geiger
+
+<img src="./logo.png" height="100">
+
+React Geiger is a tool for "audiolizing" React performance issues. You can have it running in the background and clicks will point your attention to excessive (slow) component rerenders.
+
+## Installation
+
+```bash
+npm i react-geiger
+```
+
+## Usage
+
+You wrap whatever you want to track in the `<Geiger>` component, and re-renders inside will cause a click if they take longer than the threshold set (default: 50ms).
+
+The most basic setup is wrapping your entire app:
+
+```tsx
+<Geiger>
+  <App>
+</Geiger>
+```
+
+You can also use it on a sub-tree wherever.
+
+The options are:
+
+```tsx
+  profilerId?: string;
+  renderTimeThreshold?: number;
+  phaseOption?: PhaseOption;
+  enabled?: boolean;
+```
+
+- `profilerId` is an id that will be passed on to the `React.Profiler` component. You probably don't need to change this.
+- `renderTimeThreshold` is the time in milliseconds that will trigger a click. Default is 50ms. Set to 0 to make any re-render click
+- `phaseOption` is the phase of the render you want to track, either `'mount'`, `'update'` or `'both'` (which is the default)
+- `enabled` defaults to true, but you can use this to disable it. Note that it relies on `React.Profiler` under the hood, which is disabled in production builds per default.
+
+![Not Great, Not Terrible](./not-great-not-terrible.jpg)
---
+++ src/Geiger.tsx
@@ -0,0 +1,114 @@
+import {
+  type FC,
+  Profiler,
+  type ProfilerOnRenderCallback,
+  type ReactNode,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+
+function playGeigerClickSound(audioContext: AudioContext, amplitude: number) {
+  const volume = Math.max(0.5, amplitude);
+  const duration = 0.001;
+  const startFrequency = 440 + amplitude * 200;
+
+  const oscillator = audioContext.createOscillator();
+  oscillator.type = "sine";
+  oscillator.frequency.setValueAtTime(startFrequency, audioContext.currentTime);
+  oscillator.frequency.exponentialRampToValueAtTime(
+    220,
+    audioContext.currentTime + duration
+  );
+
+  const gainNode = audioContext.createGain();
+  gainNode.gain.setValueAtTime(volume, audioContext.currentTime);
+  gainNode.gain.exponentialRampToValueAtTime(0.01, duration / 2);
+
+  // Connect the oscillator to the gain node and the gain node to the destination
+  oscillator.connect(gainNode);
+  gainNode.connect(audioContext.destination);
+
+  oscillator.start();
+  oscillator.stop(audioContext.currentTime + duration);
+}
+
+type PhaseOption = "mount" | "update" | "both";
+
+/**
+ * Geiger is a React component that plays a Geiger click sound when a component
+ * takes longer to render than a given threshold.
+ *
+ * @param {string} [profilerId="geiger"] - The id of the Profiler component.
+ * @param {number} [renderTimeThreshold=50] - The threshold in milliseconds.
+ * @param {PhaseOption} [phaseOption="both"] - The phase to listen for (mount, update, or both).
+ * @param {boolean} [enabled=true] - Whether Geiger is enabled.
+ *
+ * @example
+ * ```tsx
+ * <Geiger renderTimeThreshold={10}>
+ *  <App />
+ * </Geiger>
+ * ```
+ */
+const Geiger: FC<{
+  profilerId?: string;
+  renderTimeThreshold?: number;
+  phaseOption?: PhaseOption;
+  enabled?: boolean;
+  children: ReactNode;
+}> = ({
+  profilerId = "geiger",
+  renderTimeThreshold = 50,
+  phaseOption = "both",
+  enabled = true,
+  children,
+}) => {
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
+
+  const handleRender = useCallback<ProfilerOnRenderCallback>(
+    (_id, phase, actualDuration) => {
+      if (
+        (phase === phaseOption || phaseOption === "both") &&
+        actualDuration > renderTimeThreshold &&
+        audioContext
+      ) {
+        const amplitude = Math.min(
+          1,
+          (actualDuration - renderTimeThreshold) / (renderTimeThreshold * 2)
+        );
+        playGeigerClickSound(audioContext, amplitude);
+      }
+    },
+    [audioContext, phaseOption, renderTimeThreshold]
+  );
+
+  useEffect(() => {
+    if (enabled) {
+      const audioContext = new (window.AudioContext ||
+        // @ts-expect-error -- This is a fallback for Safari
+        window.webkitAudioContext)();
+
+      if (audioContext.state === "running") {
+        console.info(`Geiger is ${enabled ? "enabled" : "disabled"}`);
+        setAudioContext(audioContext);
+      } else {
+        console.warn(
+          "Geiger: AudioContext did not start. To enable Geiger, you need to give permission to play audio on this page."
+        );
+      }
+    }
+  }, [enabled]);
+
+  if (!enabled) {
+    return <>{children}</>;
+  }
+
+  return (
+    <Profiler id={profilerId} onRender={handleRender}>
+      {children}
+    </Profiler>
+  );
+};
+
+export default Geiger;
